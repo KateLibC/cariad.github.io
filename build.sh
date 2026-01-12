812dbbe71f0db3286293efd43abe357a82b69a93
Cariad Heather Keigher <cariad@keigher.ca>
2026-01-11 17:24:35 -0800

    updated roster

--- build.sh
+++ build.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 jekyll build
+echo "*.py\n*.sh\n.DS_*" > _site/.gitignore
 cp -R _site/* ../cariad.github.io/
 git add *
 git commit -m "$1"
-git push
+git push --no-thin 
 cd ../cariad.github.io/
 git add *
 git commit -m "$1"
-git push
+git push --no-thin
 cd ../cariad.keigher.ca
